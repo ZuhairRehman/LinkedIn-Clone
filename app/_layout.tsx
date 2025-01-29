2270866d5c085863c9c0f996c2ae6b3b6b23ea79
Zuhair Rehman <zuhair.rehman85@gmail.comgit config --global user.name Zuhair>
2025-01-29 06:32:41 +0530

    clean up before start

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,46 @@
-import { Stack } from "expo-router";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useColorScheme } from 'react-native';
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from 'expo-router';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+    const [loaded, error] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    });
+
+    // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+    useEffect(() => {
+        if (error) throw error;
+    }, [error]);
+
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
+    }
+
+    return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+    const colorScheme = useColorScheme();
+
+    return (
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+            <Stack>
+                <Stack.Screen
+                    name='(tabs)'
+                    options={{ headerShown: false }}
+                />
+                
+            </Stack>
+        </ThemeProvider>
+    );
 }
