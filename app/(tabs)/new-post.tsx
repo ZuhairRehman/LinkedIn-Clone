b1cf1471fd5e0654c986769a0f8c7c20468b0945
Zuhair Rehman <zuhair.rehman85@gmail.comgit config --global user.name Zuhair>
2025-02-02 23:22:31 +0530

    Started New post submit screen

--- app/(tabs)/new-post.tsx
+++ app/(tabs)/new-post.tsx
@@ -1,10 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, { useLayoutEffect, useState } from 'react';
+import { useNavigation, useRouter } from 'expo-router';
 
 const NewPostsScreen = () => {
+    //State Variables
+    const [content, setContent] = useState('');
+    // Hooks
+    const navigation = useNavigation();
+    const router = useRouter();
+
+    // Function to handle the post button press
+    const onPost = () => {
+        console.log(content);
+        setContent('');
+        router.back(); // Navigate back to the home screen after posting
+    };
+
+    //Routing modifications
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <TouchableOpacity
+                    onPressIn={onPost}
+                    style={styles.postButton}
+                >
+                    <Text style={styles.postBtnText}>Submit</Text>
+                </TouchableOpacity>
+            ),
+        });
+    }, [onPost]);
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>New Posts Screen</Text>
+            <TextInput
+                placeholder='what you want to talk about?..'
+                multiline
+                style={styles.input}
+                value={content}
+                onChangeText={setContent}
+            />
         </View>
     );
 };
@@ -14,11 +48,26 @@ export default NewPostsScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        padding: 15,
+    },
+
+    //Header Styles
+    postButton: {
+        backgroundColor: 'royalblue',
+        borderRadius: 50,
+        padding: 5,
+        paddingHorizontal: 15,
+        marginRight: 10,
+    },
+    postBtnText: {
+        color: 'white',
+        fontWeight: 'bold',
     },
     title: {
         fontSize: 20,
         fontWeight: 'bold',
+    },
+    input: {
+        fontSize: 18,
     },
 });
