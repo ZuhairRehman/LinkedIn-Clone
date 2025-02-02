c8ed35c98031146f4760243bfcf6b1c22dc79eb4
Zuhair Rehman <zuhair.rehman85@gmail.comgit config --global user.name Zuhair>
2025-02-03 00:00:37 +0530

    Added image picker on new psot screen

--- app/(tabs)/new-post.tsx
+++ app/(tabs)/new-post.tsx
@@ -1,10 +1,22 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import {
+    Pressable,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+    Image,
+} from 'react-native';
 import React, { useLayoutEffect, useState } from 'react';
 import { useNavigation, useRouter } from 'expo-router';
+import * as ImagePicker from 'expo-image-picker';
+import { FontAwesome } from '@expo/vector-icons';
 
 const NewPostsScreen = () => {
     //State Variables
     const [content, setContent] = useState('');
+    const [image, setImage] = useState<string | null>(null);
+
     // Hooks
     const navigation = useNavigation();
     const router = useRouter();
@@ -13,6 +25,7 @@ const NewPostsScreen = () => {
     const onPost = () => {
         console.log(content);
         setContent('');
+        setImage(null);
         router.back(); // Navigate back to the home screen after posting
     };
 
@@ -30,6 +43,19 @@ const NewPostsScreen = () => {
         });
     }, [onPost]);
 
+    // Function to handle image picking
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 5],
+            quality: 0.7,
+        });
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
+        }
+    };
+
     return (
         <View style={styles.container}>
             <TextInput
@@ -39,6 +65,37 @@ const NewPostsScreen = () => {
                 value={content}
                 onChangeText={setContent}
             />
+
+            {image && (
+                <Image
+                    source={{ uri: image }}
+                    style={styles.image}
+                />
+            )}
+
+            <Pressable
+                onPress={pickImage}
+                style={styles.footer}
+            >
+                <FontAwesome
+                    name='image'
+                    size={24}
+                    color='black'
+                    style={styles.iconButton}
+                />
+                <FontAwesome
+                    name='camera'
+                    size={24}
+                    color='black'
+                    style={styles.iconButton}
+                />
+                <FontAwesome
+                    name='glass'
+                    size={24}
+                    color='black'
+                    style={styles.iconButton}
+                />
+            </Pressable>
         </View>
     );
 };
@@ -67,7 +124,29 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold',
     },
+
+    //Input Styles
     input: {
         fontSize: 18,
+    },
+    //Image Styles
+    image: {
+        width: '100%',
+        aspectRatio: 1,
+        marginTop: 'auto',
+    },
+
+    //Footer Styles
+    footer: {
+        marginTop: 'auto',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+    iconButton: {
+        backgroundColor: 'gainsboro',
+        padding: 10,
+        borderRadius: 25,
+        overflow: 'hidden',
+        justifyContent: 'center',
     },
 });
